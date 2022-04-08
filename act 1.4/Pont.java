6ab3a7b749c4ef26f128b038d02fcc405fb31336
Azizelouni <103032007+Azizelouni@users.noreply.github.com>
2022-04-08 17:24:01 +0200

    Add files via upload

---
+++ act 1.4/Pont.java
@@ -0,0 +1,18 @@
+public class Pont {
+Voile voile;
+public int volume;
+private String couleur;
+final int IDENTIFIANT4=4;
+
+Pont(){
+}
+
+Pont(Voile voile,String couleur, final int IDENTIFIANT4){
+this.voile=voile;
+this.volume=volume;
+this.couleur=couleur;
+}
+boolean test(Boolean test){ 
+return(true);
+}
+}
